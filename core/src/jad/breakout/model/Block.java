2caa38886877e06b5e9d2c3ce9610d9e5f8c50f1
John Durham <jdurham2843@gmail.com>
2019-08-18 15:21:17 -0500

    set active variable for continued collision detection and rendering

--- core/src/jad/breakout/model/Block.java
+++ core/src/jad/breakout/model/Block.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class Block implements GameObject{
+public class Block implements GameObject {
 
     public static final int WIDTH = 40;
     public static final int HEIGHT = 40;
@@ -13,6 +13,7 @@ public class Block implements GameObject{
     private Color color = new Color(0, 1, 0, 1);
 
     private final Vector2 vector;
+    private boolean active = true;
 
     public Block(Color color, Vector2 vector) {
         this.color = color;
@@ -50,8 +51,25 @@ public class Block implements GameObject{
 
     @Override
     public void render(ShapeRenderer shapeRenderer) {
-        shapeRenderer.setColor(color);
-        shapeRenderer.rect(this.getVector().x, this.getVector().y, WIDTH, HEIGHT);
+        if (active) {
+            shapeRenderer.setColor(color);
+            shapeRenderer.rect(this.getVector().x, this.getVector().y, WIDTH, HEIGHT);
+        }
+    }
+
+    @Override
+    public boolean handleCollision(final GameObject otherObject, final float deltaTime) {
+        if (!active) {
+            return false;
+        }
+
+        final boolean collisionDetected = GameObject.super.handleCollision(otherObject, deltaTime);
+
+        if (collisionDetected) {
+            active = false;
+        }
+
+        return collisionDetected;
     }
 
 }
